d3c393ceb5a5d66982389c01eaa306f3919f0414
Alexander <cmrd-a@ya.ru>
2022-08-06 01:25:14 +0300

    Добавлена инфраструктура для тестов.

--- Makefile
+++ Makefile
@@ -2,4 +2,10 @@ black:
 	black . --line-length 120
 
 dbs:
-	docker compose up postgres redis elastic -d
+	docker compose up redis elastic -d
+
+tests_dev_up:
+	docker compose -f tests/functional/docker-compose.yml up -d
+
+tests_dev_down:
+	docker compose -f tests/functional/docker-compose.yml down
---
+++ tests/functional/src/test_film.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+class TestV1FilmsListHandler:
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("page_size", (1, 2, 50, 99))
+    async def test_film(self, create_films, make_get_request, page_size):
+        print("make request")
+
+        response = await make_get_request("/v1/films", {"page[size]": page_size})
+
+        assert response.status == 200
+        assert len(response.body["results"]) == page_size
