25dfd83f9b311267846f88906f319cdd91af41b6
n.kotov <n.kotov@cian.ru>
2022-08-09 12:33:51 +0300

    Перенес фикстуру создания тестовых жанров в папку к фикстурам.

--- tests/functional/src/test_genres_service_v1.py
+++ tests/functional/src/test_genres_service_v1.py
@@ -1,34 +1,10 @@
-import json
-
 import pytest
-import pytest_asyncio
-from elasticsearch.helpers import async_bulk
 
 from tests.functional.testdata.genres import genres
 
 
-@pytest_asyncio.fixture(name="save_genres")
-async def create_genres(es_client):
-    async def inner():
-        actions = [
-            {
-                "_index": "genres",
-                "_id": genre["id"],
-                "_source": json.dumps(genre),
-            }
-            for genre in genres
-        ]
-
-        await async_bulk(client=es_client, actions=actions, refresh=True)
-
-    return inner
-
-
 @pytest.mark.asyncio
-async def test_genres_list__genres_present__return_genres(make_get_request, save_genres):
-    # arrange
-    await save_genres()
-
+async def test_genres_list__genres_present__return_genres(make_get_request, create_genres):
     # act
     response = await make_get_request("/v1/genres")
 
@@ -47,10 +23,7 @@ async def test_genres_list__no_genres__return_status_404(make_get_request):
 
 
 @pytest.mark.asyncio
-async def test_genre_details__genres_present__return_genre(make_get_request, save_genres):
-    # arrange
-    await save_genres()
-
+async def test_genre_details__genres_present__return_genre(make_get_request, create_genres):
     # act
     response = await make_get_request("/v1/genres/1")
 
@@ -59,10 +32,7 @@ async def test_genre_details__genres_present__return_genre(make_get_request, sav
 
 
 @pytest.mark.asyncio
-async def test_genre_details__no_genre__return_status_404(make_get_request, save_genres):
-    # arrange
-    await save_genres()
-
+async def test_genre_details__no_genre__return_status_404(make_get_request, create_genres):
     # act
     response = await make_get_request("/v1/genres/100")
 
