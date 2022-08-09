b7df6992908ae9a9e591f9da2c49a156de5ee86b
n.kotov <n.kotov@cian.ru>
2022-08-09 17:49:18 +0300

    Добавил тесты для сервиса persons.

--- tests/functional/fixtures/data_gen.py
+++ tests/functional/fixtures/data_gen.py
@@ -5,6 +5,8 @@ from elasticsearch.helpers import async_bulk
 from utils.data_gen import gen_films
 
 from tests.functional.testdata.genres import genres
+from tests.functional.testdata.movies import movies
+from tests.functional.testdata.persons import persons
 
 
 @pytest_asyncio.fixture()
@@ -24,6 +26,34 @@ async def create_genres(es_client):
             "_source": json.dumps(genre),
         }
         for genre in genres
+    ]
+
+    await async_bulk(client=es_client, actions=actions, refresh=True)
+
+
+@pytest_asyncio.fixture()
+async def create_persons(es_client):
+    actions = [
+        {
+            "_index": "persons",
+            "_id": person["id"],
+            "_source": json.dumps(person),
+        }
+        for person in persons
+    ]
+
+    await async_bulk(client=es_client, actions=actions, refresh=True)
+
+
+@pytest_asyncio.fixture()
+async def create_movies(es_client):
+    actions = [
+        {
+            "_index": "movies",
+            "_id": movie["id"],
+            "_source": json.dumps(movie),
+        }
+        for movie in movies
     ]
 
     await async_bulk(client=es_client, actions=actions, refresh=True)
